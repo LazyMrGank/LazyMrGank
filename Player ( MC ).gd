5278b40e5fffb39bce96e678805f8513f3aa246e
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2023-07-31 12:27:30 +1200

    Player Animations
    done player animations

---
+++ Player ( MC ).gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+
+@export var speed = 30
+@export var jump_velocity = -400
+@export var max_speed =200
+@export var friction = 10
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$AnimatedSprite2D.play("Jump")
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		$AnimatedSprite2D.play("Run")
+		velocity.x = move_toward(velocity.x, direction * max_speed, speed)
+	else:
+		$AnimatedSprite2D.play("Idle1")
+		velocity.x = move_toward(velocity.x, 0, friction)
+		
+		
+	#Change direction
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+		
+	move_and_slide()
