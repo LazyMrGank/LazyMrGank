d7c4f2dd95640e104a814efbcdeccb883111285a
LazyMrGank <100172063+LazyMrGank@users.noreply.github.com>
2023-08-24 12:37:10 +1200

    Main world and menu
    Lost alot of brain cells from trying to make a parallax background.
    
    Will try again later.

--- Menu.gd
+++ Menu.gd
@@ -1,11 +1,11 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://Scenes/main_world.tscn")
 
 
 func _on_tutorial_area_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Tutorial Area.tscn")
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
 func _on_quit_pressed():
